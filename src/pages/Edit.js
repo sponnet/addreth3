ad8afb28fc682c027416975bff16322fcb472093
Stefaan Ponnet <stefaan@ponnet.com>
2019-06-04 14:56:23 +0200

    save images on IPFS

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -4,79 +4,55 @@ import grapesjs from "grapesjs";
 import gjspresetwebpage from "grapesjs-preset-webpage";
 
 
-class N extends Component {
-  componentDidMount() {
-    this.editor = grapesjs.init({
-      // Indicate where to init the editor. You can also pass an HTMLElement
-      plugins: [gjspresetwebpage],
-      container: "#gjs",
-      // Get the content for the canvas directly from the element
-      // As an alternative we could use: `components: '<h1>Hello World Component!</h1>'`,
-      fromElement: true,
-      // Size of the editor
-      height: "800px",
-      width: "auto",
-      // Disable the storage manager for the moment
 
-      assetManager: {
-        upload: "https://ipfs.web3.party:5001/api/v0/add?stream-channels=true",
-        multiUpload: false
-      },
+const Editor = ({ }) => {
 
-      //storageManager: { type: 'ipfs-storage' },
-      // Avoid any default panel
-      panels: { defaults: [] }
-    });
-    this.editor.on("asset:upload:response", response => {
-      debugger;
-    });
+    const onResponse = (response, editor) => {
+        debugger;
+        const d = {
+            src: `https://ipfs.web3.party:5001/api/v0/cat/${response.Hash}`,
+            type: 'image',
+            height: 100,
+            width: 200,
+        };
+        // const t = editor;
 
-    // this.editor.StorageManager.add("ipfs-storage", {
-    //   /**
-    //    * Load the data
-    //    * @param  {Array} keys Array containing values to load, eg, ['gjs-components', 'gjs-style', ...]
-    //    * @param  {Function} clb Callback function to call when the load is ended
-    //    * @param  {Function} clbErr Callback function to call in case of errors
-    //    */
-    //   load(keys, clb, clbErr) {
-    //     const result = {};
+        // const ed = this.getEditor();
+        editor.AssetManager.add(d);
+    };
 
-    //     keys.forEach(key => {
-    //       const value = SimpleStorage[key];
-    //       if (value) {
-    //         result[key] = value;
-    //       }
-    //     });
 
-    //     // Might be called inside some async method
-    //     clb(result);
-    //   },
+    React.useEffect(() => {
 
-    //   /**
-    //    * Store the data
-    //    * @param  {Object} data Data object to store
-    //    * @param  {Function} clb Callback function to call when the load is ended
-    //    * @param  {Function} clbErr Callback function to call in case of errors
-    //    */
-    //   store(data, clb, clbErr) {
-    //     for (let key in data) {
-    //       SimpleStorage[key] = data[key];
-    //     }
-    //     // Might be called inside some async method
-    //     clb();
-    //   }
-    // });
-  }
 
-  render() {
+        const editor = grapesjs.init({
+            // Indicate where to init the editor. You can also pass an HTMLElement
+            plugins: [gjspresetwebpage],
+            container: "#gjs",
+            fromElement: true,
+
+            height: "800px",
+            width: "auto",
+
+            assetManager: {
+                upload: "https://ipfs.web3.party:5001/api/v0/add?stream-channels=true",
+                multiUpload: false,
+            },
+            panels: { defaults: [] }
+        });
+
+        editor.on("asset:upload:response", (response) => { onResponse(response, editor); });
+
+
+    }, []);
+
     return (
-      <div>
-        <div id="gjs">
-          <h1>Hello World Component!</h1>
+        <div>
+            <div id="gjs">
+            </div>
         </div>
-      </div>
     );
-  }
+
 }
 
-export default N;
+export default Editor;
