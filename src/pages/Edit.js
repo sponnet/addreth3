0bacb227df5ce02046279e2f75404295bad17906
Stefaan Ponnet <stefaan@ponnet.com>
2019-06-04 17:04:08 +0200

    DeoCities.org is born I guess

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,47 +1,68 @@
-import React, { Component } from "react";
+import React from "react";
 import "grapesjs/dist/css/grapes.min.css";
 import grapesjs from "grapesjs";
 import gjspresetwebpage from "grapesjs-preset-webpage";
-
-
+import IPFS from "ipfs-mini";
 
 const Editor = ({ }) => {
 
-    const onResponse = (response, editor) => {
-        debugger;
+    const ipfsEndpoint = "https://ipfs.web3.party:5001/api/v0";
+    const ipfs = new IPFS({ host: 'ipfs.web3.party', port: 5001, protocol: 'https' });
+    const ipfsGatewayEndpoint = "https://ipfs.web3.party/ipfs";
+
+    // upload asset to IPFS and add URL to asset manager
+    const uploadAsset = (response, editor) => {
         const d = {
-            src: `https://ipfs.web3.party:5001/api/v0/cat/${response.Hash}`,
+            src: `${ipfsEndpoint}/cat/${response.Hash}`,
             type: 'image',
             height: 100,
             width: 200,
         };
-        // const t = editor;
-
-        // const ed = this.getEditor();
         editor.AssetManager.add(d);
     };
 
-
     React.useEffect(() => {
-
-
         const editor = grapesjs.init({
             // Indicate where to init the editor. You can also pass an HTMLElement
             plugins: [gjspresetwebpage],
             container: "#gjs",
             fromElement: true,
-
-            height: "800px",
-            width: "auto",
-
             assetManager: {
-                upload: "https://ipfs.web3.party:5001/api/v0/add?stream-channels=true",
+                upload: `${ipfsEndpoint}/add?stream-channels=true`,
                 multiUpload: false,
+                credentials: 'omit',
             },
-            panels: { defaults: [] }
+            panels: { defaults: [] },
         });
 
-        editor.on("asset:upload:response", (response) => { onResponse(response, editor); });
+        // enable saving of assets (images) to IPFS 
+        editor.on("asset:upload:response", (response) => { uploadAsset(response, editor); });
+
+        // add panel with IPFS save button
+        var newPanel = editor.Panels.addPanel({
+            id: 'myIPFSSaver',
+            visible: true,
+            buttons: [{
+                id: 'alert-button',
+                className: 'btn-alert-button',
+                label: 'Publish to IPFS',
+                command(editor) {
+                    // create HTML output
+                    const output = `<html><head><style>${editor.getCss()}</style></head><body>${editor.getHtml()}</body></html>`;
+
+                    // save HTML to IPFS
+                    ipfs.add(output).then((result) => {
+                        editor.Modal.setContent(`<div>Your site is live at<br/><h4>${ipfsGatewayEndpoint}/${result}</h4></div>`);
+                        editor.Modal.open();
+                    }).catch((e) => {
+                        editor.Modal.setContent(`<div>Could not save your page ${e.message}</div>`);
+                        editor.Modal.open();
+                    });
+                }
+            }],
+        });
+
+
 
 
     }, []);
